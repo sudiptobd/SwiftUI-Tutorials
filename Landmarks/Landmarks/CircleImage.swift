950c980deb1d747c8062453cdbb8b7b0a285b77e
Sudipto Roy <sudipto@brainstation-23.com>
2024-03-09 15:25:02 +0600

    Section 4: Create a custom image view

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sudipto Roy on 9/3/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
