d14996ace9e5afafc1ea010356b052ba50561909
Sudipto Roy <sudipto@brainstation-23.com>
2024-04-01 00:52:48 +0600

    Section 5: Use SwiftUI views from other frameworks

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,33 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Sudipto Roy on 1/4/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+}
+
+
+private var region: MKCoordinateRegion {
+    MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 34.011_286,
+            longitude: -116.166_868
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.2
+        )
+    )
+}
+
+#Preview {
+    MapView()
+}
