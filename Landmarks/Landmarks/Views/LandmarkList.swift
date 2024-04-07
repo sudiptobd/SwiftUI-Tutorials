2731f4b0ff55794d155bc8a33b612dd4f9d2aa1a
Sudipto Roy <sudipto@brainstation-23.com>
2024-04-07 16:33:19 +0600

    Section 4: Building lists and navigation: Create the list of landmarks

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Sudipto Roy on 7/4/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
