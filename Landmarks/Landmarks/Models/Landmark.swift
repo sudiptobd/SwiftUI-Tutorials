b36bded775d459c508f9be5892fb051aa7ab89db
Sudipto Roy <sudipto@brainstation-23.com>
2024-04-12 11:32:59 +0600

    Handling User Input: Section 1: Mark Favourite Landmarks

--- Landmarks/Landmarks/Models/Landmark.swift
+++ Landmarks/Landmarks/Models/Landmark.swift
@@ -15,6 +15,9 @@ struct Landmark: Hashable, Codable, Identifiable {
     var park: String
     var state: String
     var description: String
+    var isFavourite: Bool {
+        true
+    }
     
     private var imageName: String
     var image: Image {
